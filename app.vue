84353b8b41c0602bf9b83bbc34f35f669972afb6
yusufstawan <yusufsetiyawan45@gmail.com>
2023-03-26 05:20:27 +0700

    add and configure tailwind css

--- app.vue
+++ app.vue
@@ -1,5 +1,3 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ["~/assets/main.scss"],
 
-})
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
