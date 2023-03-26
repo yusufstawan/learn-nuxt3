b2ecc5c3dec22b75ea45da85de19034d91be0887
yusufstawan <yusufsetiyawan45@gmail.com>
2023-03-26 12:01:29 +0700

    assets, public folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  alias: {
+    assets: "/<rootDir>/assets",
+  },
+
   css: ["~/assets/main.scss"],
 
   postcss: {
