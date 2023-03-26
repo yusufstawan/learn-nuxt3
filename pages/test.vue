910f408ce3f30d026e5aa65a008019906c96a020
yusufstawan <yusufsetiyawan45@gmail.com>
2023-03-26 22:41:54 +0700

    fetch data

--- pages/test.vue
+++ pages/test.vue
@@ -1,5 +1,12 @@
+<script setup>
+const { data: api } = await useFetch("https://jsonplaceholder.typicode.com/todos/1");
+</script>
+
 <template>
   <main>
     <ContentDoc />
+    <div>
+      {{ api }}
+    </div>
   </main>
 </template>
